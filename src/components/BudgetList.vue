169885cdc117bba6d9f4b1f059b6cefa431cb25b
Thijs Kuilman <thijs@yukka.nl>
2018-10-20 14:24:40 +0200

    Added category object

--- src/components/BudgetList.vue
+++ src/components/BudgetList.vue
@@ -5,23 +5,32 @@
         <tr>
           <th>Name</th>
           <th>Budget</th>
+          <th>Activity</th>
+          <th>Available</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="category in categories">
           <td>
-            {{ category }}
+            {{ category.name }}
           </td>
           <td>
             <input type="number" value="0">
           </td>
+          <td>
+            0
+          </td>
+          <td>
+            <span class="badge badge-success">
+              321
+            </span>
+          </td>
         </tr>
-      
       </tbody>
     </table>
 
     <form v-on:submit.prevent="addCategory">
-      <input autofocus type=text v-model="categoryName"></input>
+      <input autofocus type=text v-model="newCategory.name"></input>
       <button class="btn btn-primary ml-3">Add category</button>
     </form>
   </main>
@@ -32,15 +41,30 @@ export default {
   name: 'BudgetList',
   data() {
     return {
-      categories: [],
-      categoryName: '',
+      categories: [
+        {
+          name: 'Boodschappen',
+          budgeted: 200,
+        },
+        {
+          name: 'Spotify',
+          budgeted: 10,
+        },
+      ],
+      newCategory: {
+        name: '',
+        budgeted: 0,
+      }
     }
   },
   methods: {
     addCategory() {
-      if(this.categoryName.length > 0) {
-        this.categories.push(this.categoryName);
-        this.categoryName = '';
+      if(this.newCategory.name.length > 0) {
+        this.categories.push({
+          name: this.newCategory.name,
+          budgeted: 0,
+        });
+        this.newCategory.name = '';
       }
     }
   }
